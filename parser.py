de395b99c9c3fd80dba682cd33b5968435d0a171
Licheng Xiao <31831988+LichengXiao2017@users.noreply.github.com>
2018-03-24 01:52:53 -0700

    Update parser.py

--- parser.py
+++ parser.py
@@ -70,6 +70,6 @@ class Parser(object):
 
         # http://stackoverflow.com/a/3732128/1410871
         img = Image.new(mode='L', size=(width, height), color=0)
-        ImageDraw.Draw(img).polygon(xy=polygon, outline=1, fill=0)
+        ImageDraw.Draw(img).polygon(xy=polygon, outline=0, fill=1)
         mask = np.array(img).astype(bool)
         return mask
